53ffca0e05a8ab23fb3b3561a8eaccf3d042ac80
Jefferson Perluxo Clemente <jperluxo@gmail.com>
2025-08-15 19:38:49 -0300

    Componentização do Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./index.module.css";
+
+import { ReactComponent as Logo } from "../../images/logo.svg";
+import { Link } from "react-router-dom";
+import { IoMdDownload } from "react-icons/io";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <Logo className={styles.logo}/>
+      <nav>
+        <Link to="/">Início</Link>
+        <Link to="/about">Sobre</Link>
+        <Link to="/projects">Projetos</Link>
+      </nav>
+      <button type="button"><IoMdDownload/>Baixar CV</button>
+    </header>
+  )
+}
+
+export default Header;
