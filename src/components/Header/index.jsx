ca2aa777c3fb097138e0db883f8cd7db535158db
Jefferson Perluxo Clemente <jperluxo@gmail.com>
2025-09-02 20:26:31 -0300

    Implementação do modo claro; Ajuste no arquivo css da Home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import styles from "./index.module.css";
 
 import { ReactComponent as Logo } from "../../images/logo.svg";
@@ -6,11 +6,18 @@ import { Link } from "react-router-dom";
 import { IoMdDownload } from "react-icons/io";
 
 const Header = () => {
+  const [theme, setTheme] = useState("dark");
   const handleDownload = () => {
     const link = document.createElement("a");
     link.href = "/Curriculum_Vitae.pdf";
     link.click();
   };
+
+  useEffect(() => {
+    document.documentElement.className = theme === "light" ? "lightTheme" : "";
+  }, [theme]);
+
+  const Tooltip = `Modo ${theme === "light" ? "escuro" : "claro"}`;
 
   return (
     <header className={styles.header}>
@@ -20,7 +27,13 @@ const Header = () => {
         <Link to="/about">Sobre</Link>
         <Link to="/projects">Projetos</Link>
       </nav>
-      <button type="button" onClick={handleDownload}><IoMdDownload aria-hidden={true}/>Baixar CV</button>
+      <div>
+        <button type="button" onClick={handleDownload}><IoMdDownload aria-hidden={true}/>Baixar CV</button>
+        <label>
+          <input type="checkbox" role="switch" onChange={() => setTheme(theme === "light" ? "dark" : "light")} checked={theme === "light"} title={Tooltip} name="isLightMode"/>
+          <span title={Tooltip}/>
+        </label>
+      </div>
     </header>
   )
 }
